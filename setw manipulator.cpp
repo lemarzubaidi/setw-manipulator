83e0dab9aa3ca08225e646180e62aca94b804506
Lemar Zubaidi <lemarzubaide9@gmail.com>
2025-05-13 21:54:31 +0300

    a little example

---
+++ setw manipulator.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	cout << "-----------|---------------------------------|----------|\n";
+	cout << "    Code   |               Name              |    Mark  |\n";
+	cout << "-----------|---------------------------------|----------|\n";
+	cout << setw(11) << "C101" << "|" << setw(33) << "introduction to programming 1" << "|" << setw(10) << "95" << "|" << endl;
+	cout << setw(11) << "C102" << "|" << setw(33) << "computer hardware" << "|" << setw(10) << "88" << "|" << endl;
+	cout << setw(11) << "C1035243" << "|" << setw(33) << "Network" << "|" << setw(10) << "75" << "|" << endl;
+
+}
+
